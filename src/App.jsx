800ac01d5757bd3a388de62c6ffd48370aaaf91c
takbirgazi <takbirgazibd@gmail.com>
2024-03-16 12:39:33 +0600

    Complete halp of design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Heading from './components/Heading/Heading'
 import Hero from './components/Hero/Hero'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className='w-[85%] mx-auto'>
         <Header></Header> 
         <Hero></Hero>
+        <Heading></Heading>
       </div>
     </>
   )
--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,7 +1,8 @@
 export default function Heading(){
     return(
-        <div>
-            Header
+        <div className="flex flex-col items-center justify-center gap-1 my-5 lg:w-2/3 mx-auto">
+           <h3 className="text-2xl font-bold">Our Recipes</h3>
+           <p className="text-center">Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus vulputate netus pharetra rhoncus. Eget urna volutpat curabitur elementum mauris aenean neque.</p>
         </div>
     )
 }
